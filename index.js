b35e1830096d526621c5cae15c7ff31fe7d262d1
FahemAME <80149350+FahemAME@users.noreply.github.com>
2021-03-06 14:40:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express=require("express");
 const app=express();
 const importdata=require("./data.json");
-let port =process.env.port || 3000;
+let port =process.env.port || 3011;
 app.get("/",(req,res)=>{
-	res.send("Hiiiii there...");
+	res.send("Hiiiii5..3012... there...");
 	});
 app.get("/data",(req,res)=>{
 	res.send(importdata);
